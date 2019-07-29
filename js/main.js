9183fb36e5a886b996b1a1844e6d01294315874e
NatalieSnegireva <snegireva2208@outlook.com>
2019-07-29 14:59:52 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 var swiper = new Swiper('.swiper-container', {
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + (index + 1) + '</span>';
-        },
-      },
-    });
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+});
+
+$(document).ready(function(){
+  $('.nav-toggle').on('click', function(e){
+    e.preventDefault();
+    $('.block-nav').toggleClass('is-open')
+  })
+})
